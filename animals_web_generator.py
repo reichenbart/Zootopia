ca273f5527d957a492187933cadde6554b5f5301
Jan <janreichenbach1991@gmail.com>
2026-09-14 13:26:37 +0200

    bring animal data into html

--- animals_web_generator.py
+++ animals_web_generator.py
@@ -2,33 +2,44 @@ import json
 
 
 
-def load_data(file_path):
+def load_json_data(file_path):
     """loads a JSON file"""
     with open(file_path, "r") as handle:
         return json.load(handle)
 
 
-animals_data = load_data('animals_data.json')
+animals_data = load_json_data('animals_data.json')
 
 
 
 def get_info(animals):
     """prints specific info of a dict in a list"""
+    output = ""
     for animal in animals:
         locations = ", ".join(animal.get("locations", []))
 
-        print(
-            "Name:", animal.get('name'),"\n"
-            "Diet:", animal.get('characteristics').get('diet'),"\n"
-            "Location:", locations
-        )
+        output += f"Name: {animal.get('name')}\n"
+        output += f"Diet: {animal.get('characteristics').get('diet')}\n"
+        output += f"Location: {locations}\n"
 
         if animal.get('characteristics', {}).get('type'):
-            print(
-                "Type:", animal['characteristics']['type']
-            )
+            output += f"Type: {animal['characteristics']['type']}\n"
 
-        print()
+        output += "\n"
+
+    return output
 
 
-print(get_info(animals_data))
+
+fox_info = get_info(animals_data)
+
+
+
+with open("animals_template.html", "r") as html_file:
+    page = html_file.read()
+
+
+animals_html = page.replace("__REPLACE_ANIMALS_INFO__", fox_info)
+
+with open("animals.html", "w") as html_file:
+    new_page = html_file.write(animals_html)
