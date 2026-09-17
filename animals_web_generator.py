7b219747d12691fd1182fecce96854007f9e2272
Jan <janreichenbach1991@gmail.com>
2026-09-17 14:27:26 +0200

    implemented pep8

--- animals_web_generator.py
+++ animals_web_generator.py
@@ -8,9 +8,6 @@ def load_json_data(file_path):
         return json.load(handle)
 
 
-animals_data = load_json_data('animals_data.json')
-
-
 
 def get_info(animals):
     """prints specific info of a dict in a list"""
@@ -33,15 +30,31 @@ def get_info(animals):
 
 
 
-fox_info = get_info(animals_data)
+def read_template(file_path):
+    with open(file_path, "r") as html_file:
+        page = html_file.read()
+    return page
 
 
 
-with open("animals_template.html", "r") as html_file:
-    page = html_file.read()
+def write_template(content, file_path):
+    with open(file_path, "w") as html_file:
+        new_page = html_file.write(content)
+    return new_page
 
 
-animals_html = page.replace("__REPLACE_ANIMALS_INFO__", fox_info)
 
-with open("animals.html", "w") as html_file:
-    new_page = html_file.write(animals_html)
+def main():
+    animals_data = load_json_data('animals_data.json')
+    fox_info = get_info(animals_data)
+    starting_page = "animals_template.html"
+
+    page = read_template(starting_page)
+    animals_html = page.replace("__REPLACE_ANIMALS_INFO__", fox_info)
+
+    write_template(animals_html, "animals.html")
+
+
+
+if __name__ == "__main__":
+    main()
